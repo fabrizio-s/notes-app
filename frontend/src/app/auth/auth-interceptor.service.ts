74812c4648763a792d25ace803b3c35ab9cfd502
Fabrizio Silva <r.dipietro@reply.it>
2019-06-30 11:11:38 +0200

    frontend - read-fast auth-interceptor.service

--- frontend/src/app/auth/auth-interceptor.service.ts
+++ frontend/src/app/auth/auth-interceptor.service.ts
@@ -8,13 +8,13 @@ export class AuthInterceptorService implements HttpInterceptor {
     constructor(private authService: AuthService) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler) {
-        if (this.authService.user.getValue()) {
-            const authorizedRequest = request.clone({
-                headers: request.headers.append('Authorization', this.authService.user.getValue().token)
-            });
-            return next.handle(authorizedRequest);
+        if (!this.authService.user.getValue()) {
+            return next.handle(request);
         }
-        return next.handle(request);
+        const authorizedRequest = request.clone({
+            headers: request.headers.append('Authorization', this.authService.user.getValue().token)
+        });
+        return next.handle(authorizedRequest);
     }
 
 }
