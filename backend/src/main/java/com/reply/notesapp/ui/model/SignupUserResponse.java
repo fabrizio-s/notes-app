ac864c25932e44a1f8f6180bff907e84b39db587
Fabrizio Silva <r.dipietro@reply.it>
2019-07-30 09:32:42 +0200

    updated.

---
+++ backend/src/main/java/com/reply/notesapp/ui/model/SignupUserResponse.java
@@ -0,0 +1,45 @@
+package com.reply.notesapp.ui.model;
+
+public class SignupUserResponse {
+	
+	private Long id;
+	
+	private String username;
+	
+	private String email;
+	
+	private boolean enabled;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+}
